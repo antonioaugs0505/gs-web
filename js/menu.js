c67b5187b51f5557213bd632b17dabd10f9eb1eb
antonioaugs0505 <rm550344@fiap.com.br>
2023-06-07 01:53:06 -0300

    menu com js

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+let btn = document.getElementById('btn')
+btn.addEventListener('click', ()=>{
+
+let menuMobile = document.querySelector('.mobile-menu')
+if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open')
+    document.querySelector('.icon').src = "../img/logo-menu/menu-aberto.png"
+} else{
+    menuMobile.classList.add('open')
+    document.querySelector('.icon').src = "../img/logo-menu/menu-fechado.png"
+}
+
+
+})
